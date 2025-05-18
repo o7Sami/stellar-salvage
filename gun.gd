5a04eb9199e75ae5f4e9fdb96ad12e5ca522cf4a
o7Sami <bakrim.sami@gmail.com>
2025-05-18 21:26:51 +0200

    waffe nach scheiß tutorial (funktioniert nicht)
    ich hasse alles ich will nie wieder programmieren wenn ich noch einmal info habe muss ich meine anti depressiva höher dosieren

---
+++ gun.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+var bullet_path=preload("res://Bullet.tscn")
+
+func _physics_process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("left_mouse"):
+		fire()
+
+func fire():
+	var bullet = bullet_path.instantiate()
+	bullet.dir=rotation
+	bullet.pos=$Node2D.global_position
+	bullet.rota = global_rotation
+	get_parent().add_child(bullet)
+	
