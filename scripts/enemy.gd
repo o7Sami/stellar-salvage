78258c7fdbbd6bf8ac86afcb5ec288d904963b8a
Castell0o <jonas@burg-web.de>
2025-05-18 13:56:43 +0200

    Enemy + Pathfinding
    Enemy scene hinzugefügt und ein pathfinding zum spieler gegeben, agro hitbox fehlt

---
+++ scripts/enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const speed = 20
+@export var player: Node2D
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+
+func _physics_process(delta: float) -> void:
+	var direction = to_local(navigation_agent_2d.get_next_path_position()).normalized()
+	velocity = direction * speed
+	move_and_slide()
+
+func makePath() -> void:
+	navigation_agent_2d.target_position = player.global_position
+
+
+
+
+func _on_timer_timeout() -> void:
+	makePath()
