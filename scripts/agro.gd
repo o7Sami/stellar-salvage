5c8a7627a86720fc047ee32589cd657e7489135f
Castell0o <jonas@burg-web.de>
2025-05-19 21:09:53 +0200

    agro funktioniert
    agro funktioniert

--- scripts/agro.gd
+++ scripts/agro.gd
@@ -1,9 +1,15 @@
 extends Area2D
-
-var da: bool = false
-
+@onready var timer: Timer = $Timer
+@export var enemy: Node2D
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	da = true
-	print("g")
+	enemy.agrovated = 1
+	timer.start()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	enemy.agrovated = 0
+
+func _on_timer_timeout() -> void:
+	enemy.makePath()
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,21 +1,17 @@
 extends CharacterBody2D
 
+var agrovated = 0
 const speed = 20
 @export var player: Node2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
-@onready var agro: Area2D = $Agro
+
 
 func _physics_process(delta: float) -> void:
-	if agro.da == true:
+	if agrovated == 1:
 		var direction = to_local(navigation_agent_2d.get_next_path_position()).normalized()
 		velocity = direction * speed
 		move_and_slide()
+	
 
 func makePath() -> void:
 	navigation_agent_2d.target_position = player.global_position
-
-
-
-
-func _on_timer_timeout() -> void:
-	makePath()
