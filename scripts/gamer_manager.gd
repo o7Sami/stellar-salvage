74749be6db6496baad2e3f9ba29b0fbb84117d01
jakobschlueter <jakob.anony@gmx.de>
2025-05-20 19:40:56 +0200

    Add files via upload

--- scripts/gamer_manager.gd
+++ scripts/gamer_manager.gd
@@ -1,11 +1,13 @@
 extends Node
 
+@onready var schlüsselanzahl: Label = %Schlüsselanzahl
+
 var keys = 0
 
 func add_keys():
 	keys += 1
-	print(keys)
+	schlüsselanzahl.text = str(keys)
 
 func sub_keys():
 	keys -= 1
-	print(keys)
+	schlüsselanzahl.text = str(keys)
