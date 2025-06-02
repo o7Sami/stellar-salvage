451412e92838c30145c84dcb75e20f22362d8352
o7Sami <bakrim.sami@gmail.com>
2025-06-02 13:32:00 +0200

    Delete stellar-salvage-abdeckung directory

--- stellar-salvage-abdeckung/scripts/sichtfeld_begrenzung.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-
-func _on_delete_zone_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
--- stellar-salvage-abdeckung/scripts/tür_waagrecht.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-var offen = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta: float) -> void:
-#	if offen == true:
-		
