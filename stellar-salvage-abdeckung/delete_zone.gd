fa655a492761ebd57e4887947489c9cb06d435da
f-d-inf <felix-diller@web.de>
2025-06-02 13:35:36 +0200

    Delete stellar-salvage-abdeckung directory

--- stellar-salvage-abdeckung/delete_zone.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-const PLAYER = preload("res://scenes/player.tscn")
-@onready var abdeckung: Sprite2D = $"../abdeckung"
-
-
-
-	
-func _on_body_entered(_player) -> void:
-	abdeckung.hide()
