bad454f07ac86e292455c3b82a27f3ba0e140512
jakobschlueter <jakob.anony@gmx.de>
2025-06-25 08:24:36 +0200

    Add files via upload

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,30 +1,14 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
-
-func _on_options_pressed() -> void:
-	print("options")
-
-
-func _on_exit_pressed() -> void:
-	#print("exit")
-	get_tree().quit()
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-
-
 func _on_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 	print("tutorial")
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_optionen_pressed() -> void:
+	print("options")
+
+func _on_verlassen_pressed() -> void:
+	get_tree().quit()
---
+++ scripts/t├╝r_senkrecht.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+var offen = false
+
+@onready var game_manager: Node = %GameManager
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if offen == true:
+		animated_sprite_2d.play("offen")
+		set_collision_layer_value(1,false)
+	else:
+		animated_sprite_2d.play("geschlossen")
+		set_collision_layer_value(2,false)
+
+func _on_öffnungsbereich_body_entered(body: Node2D) -> void:
+	if game_manager.keys > 0 and offen == false:
+			offen = true
+			game_manager.sub_keys()
