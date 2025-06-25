7cf82602172473a165992b281a3b97fa22a5bb49
Castell0o <jonas@burg-web.de>
2025-06-25 09:00:12 +0200

    Revert "Revert "Lebensystem endlich""

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,9 @@
 extends Node2D
-@onready var player: CharacterBody2D = $Player
-@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
-func _process(_delta: float) -> void:
-	canvas_layer.changeLife(player.Health)
+@onready var player: CharacterBody2D = $Player
+@onready var hearts_container: HBoxContainer = $CanvasLayer/HeartsContainer
+
+func _ready() -> void:
+	hearts_container.setMaxHealth(player.Maxhealth)
+	hearts_container.updateHearts(player.currentHealth)
+	player.Healthchanged.connect(hearts_container.updateHearts)
