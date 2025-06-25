8395e5564c683b62bc354fd9b08276271e862119
jakobschlueter <jakob.anony@gmx.de>
2025-06-25 09:11:30 +0200

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,3 +1,16 @@
 extends Control
 
 
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+	print("tutorial")
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_optionen_pressed() -> void:
+	print("options")
+
+func _on_verlassen_pressed() -> void:
+	get_tree().quit()
+
