aa26ab8f745ecec4b9ddbd5fc108423a047ae59a
o7Sami <bakrim.sami@gmail.com>
2025-06-25 08:41:21 +0200

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,3 @@
 extends Control
 
 
-
-func _on_options_pressed() -> void:
-	print("options")
-
-
-func _on_exit_pressed() -> void:
-	#print("exit")
-	get_tree().quit()
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
