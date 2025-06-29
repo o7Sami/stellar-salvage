31189eb09d248b61c820b6b960a4e1e01a3dfb6a
o7Sami <bakrim.sami@gmail.com>
2025-06-29 15:19:41 +0200

    agerui90ß

--- node_2d.gd
+++ node_2d.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 var push = false
 var direction=0
+var directiony =0
 const PLAYER = preload("res://scenes/player.tscn")
 
 func _physics_process(delta: float) -> void:
 	if push:
-		
-		velocity.x =direction*delta*500
+		velocity.y =directiony*delta*500
+		velocity.x =direction*delta*700
+		move_and_slide()
 	else:
 		velocity.x = 0
-	
+		velocity.y = 0
+		move_and_slide()
 	
 	
 	
@@ -17,7 +20,7 @@ func _physics_process(delta: float) -> void:
 func _on_rechts_body_entered(body:) -> void:
 	direction= -1
 	push= true
-	print("ho^^")
+
 
 
 func _on_rechts_body_exited(PLAYER) -> void:
@@ -30,4 +33,24 @@ func _on_links_body_entered(PLAYER) -> void:
 
 func _on_links_body_exited(PLAYER) -> void:
 	direction= 0
+	push= false
+
+
+func _on_oben_body_entered(body: Node2D) -> void:
+	directiony= 1 
+	push= true
+
+
+func _on_oben_body_exited(body: Node2D) -> void:
+	directiony= 0
+	push= false
+
+
+func _on_unten_body_entered(body: Node2D) -> void:
+	directiony= -1 
+	push= true
+
+
+func _on_unten_body_exited(body: Node2D) -> void:
+	directiony= 0
 	push= false
