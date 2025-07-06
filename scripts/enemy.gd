47cf337f4646d60da144a5832de9ec0640503281
Castell0o <jonas@burg-web.de>
2025-07-07 00:47:05 +0200

    resources

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
+@export var enemy_stats:Resource
+
 var agrovated = 0
-const speed = 20
 @export var player: Node2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
+func _ready():
+	set_modulate(enemy_stats.appearance())
 
 func _physics_process(delta: float) -> void:
 	if agrovated == 1:
 		var direction = to_local(navigation_agent_2d.get_next_path_position()).normalized()
-		velocity = direction * speed
+		velocity = direction * enemy_stats.speed
 		move_and_slide()
 	
 
