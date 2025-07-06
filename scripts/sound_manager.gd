ad6215b8cda975f52d84090f0ce3f2c6da767991
o7Sami <bakrim.sami@gmail.com>
2025-07-06 23:29:45 +0200

    Sounds und co

---
+++ scripts/sound_manager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func Hintergrund_abspielen():
+	$hintergrund.play()
+
+func Schluessel_abspielen():
+	$Schluessel.play()
+
+func Tuer_abspielen():
+	$Tuer.play()
+
+func Boxenschieben_abspielen():
+	$Boxenschieben.play()
+
+func Teleporter_abspielen():
+	$Teleporter.play()
