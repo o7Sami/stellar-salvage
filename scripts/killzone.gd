5d9be1ec6140fbd5703e5e8918d5be5d6f1083a2
Castell0o <jonas@burg-web.de>
2025-07-06 23:15:31 +0200

    ja

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,10 +4,10 @@ extends Area2D
 #@onready var player2: CharacterBody2D = $"."
 
 func _on_body_entered(_player) -> void:
-	print("You Died!") 
-	timer.start()
-	#player2.tot = true
-
+	_player.currentHealth -= 1
+	_player.healthChanged.emit(_player.currentHealth)
+	if _player.currentHealth < 1:
+		timer.start()
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/endscreen.tscn")
