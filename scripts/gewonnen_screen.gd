54cac0ed1b806b8f705f50496e8794ccc25e1b0c
Castell0o <jonas@burg-web.de>
2025-07-07 18:28:03 +0200

    welt zu welt

--- scripts/gewonnen_screen.gd
+++ scripts/gewonnen_screen.gd
@@ -1,21 +1,18 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_spiel_starten_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	Global.aktuelle_Scene = 0
-	Global.Raumschiffteile_gesamt = 0
 
 
 func _on_hauptmenü_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_spiel_starten_pressed() -> void:
+	print("_______________________________________________________")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	Global.aktuelle_Scene = 0
+	Global.Raumschiffteile_gesamt = 0
+	Global.currentHealth = Global.MaxHealth
+	print("_______________________________________________________")
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,4 +2,5 @@ extends Node
 
 var Raumschiffteile_gesamt = 0
 var aktuelle_Scene = 0
-var currentHealth = 5
+@export var MaxHealth: int = 5
+@export var currentHealth: int = MaxHealth
