3d9ed6783769c6e19875e4d799426484da878336
o7Sami <bakrim.sami@gmail.com>
2025-07-07 21:47:55 +0200

    oiuzhswegf

--- scripts/schluessel.gd
+++ scripts/schluessel.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-@onready var game_manager: Node = %GameManager
+
+
 
 func _on_body_entered(body):
-		game_manager.add_keys()
+		Global.add_keys()
 		SoundManager.Schluessel_abspielen()
 		queue_free()
