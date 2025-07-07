21bf8916ecea7f02ac0ed3f22f452ed72104b22d
o7Sami <bakrim.sami@gmail.com>
2025-07-07 19:29:20 +0200

    geg

--- scripts/Enemy_Resource.gd
+++ scripts/Enemy_Resource.gd
@@ -4,7 +4,7 @@ class_name enemy_type
 @export var name:String
 @export var type: types
 @export var speed: float
-@export var health: float
+@export var health: int
 #@export var texture: Texture2D
 @export var dropKey: bool
 
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -13,8 +13,11 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 
-func _on_body_entered(body: Node2D) -> void:
-	#if body.is_in_group("Enemy"):
-		#body.get_damage(damage)
-		#queue_free()
+func _on_body_entered(_Enemy:CharacterBody2D) -> void:
+	_Enemy.schaden()
+	queue_free()
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	queue_free()
