183f04f1158c7abb2a430490928841a07d4230f1
Castell0o <jonas@burg-web.de>
2025-07-08 01:34:44 +0200

    fertig

---
+++ scenes/tutorial.gd
@@ -0,0 +1,14 @@
+extends Node2D
+ 
+
+@onready var hearts_container: HBoxContainer = $CanvasLayer/heartsContainer
+@onready var player: CharacterBody2D = $Player
+
+
+func _ready() -> void:
+	hearts_container.setMaxHearts(Global.MaxHealth)
+	Global.healthChanged.connect(hearts_container.updateHearts)
+	Global.maxChanged.connect(hearts_container.setMaxHearts)
+ 
+func _process(delta: float) -> void:
+	hearts_container.updateHearts(Global.currentHealth)
