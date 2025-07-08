e2f060995dda73379bc7df771a3e01c54cdea94c
f-d-inf <felix-diller@web.de>
2025-07-09 01:00:10 +0200

    RRR

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -6,8 +6,8 @@ const PLAYER = preload("res://scenes/player.tscn")
 
 func _physics_process(delta: float) -> void:
 	if push:
-		velocity.y =directiony*delta*500
-		velocity.x =direction*delta*700
+		velocity.y =directiony*delta*1200
+		velocity.x =direction*delta*1200
 		move_and_slide()
 	else:
 		velocity.x = 0
@@ -36,22 +36,22 @@ func _on_links_body_exited(PLAYER) -> void:
 	push= false
 
 
-func _on_oben_body_entered(body: Node2D) -> void:
+func _on_oben_body_entered(PLAYER) -> void:
 	directiony= 1 
 	push= true
 
 
-func _on_oben_body_exited(body: Node2D) -> void:
+func _on_oben_body_exited(PLAYER) -> void:
 	directiony= 0
 	push= false
 
 
-func _on_unten_body_entered(body: Node2D) -> void:
+func _on_unten_body_entered(PLAYERD) -> void:
 	directiony= -1 
 	push= true
 
 
-func _on_unten_body_exited(body: Node2D) -> void:
+func _on_unten_body_exited(PLAYER) -> void:
 	directiony= 0
 	push= false
 
