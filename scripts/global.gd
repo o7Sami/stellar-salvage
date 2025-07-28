416b97e64a527a2e789d305da94a7cf99e70134a
jakobschlueter <jakob.anony@gmx.de>
2025-07-28 13:01:57 +0200

    Add files via upload

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,14 +1,25 @@
 extends Node
 
+signal healthChanged
+signal maxChanged
+var Schuesse: int = 30
+var keys = 0
+var Schwierigkeit = 1
 var Raumschiffteile_gesamt = 0
 var aktuelle_Scene = 0
 var startHealth: int = 5
 @export var MaxHealth: int = startHealth
 @export var currentHealth: int = MaxHealth
-signal healthChanged
-signal maxChanged
-var Schuesse: int = 30
-var keys = 0
+
+func Beginn():
+	if Schwierigkeit == 0:
+		startHealth = 7
+	if Schwierigkeit == 1:
+		startHealth = 5
+	if Schwierigkeit == 2:
+		startHealth = 3
+	MaxHealth = startHealth
+	currentHealth = MaxHealth
 
 func add_keys():
 	keys += 1
@@ -19,7 +30,12 @@ func sub_Schuesse():
 	Schuesse -= 1
 
 func Schuesse_reset():
-	Schuesse = 30
+	if Schwierigkeit == 0:
+		Schuesse = 50
+	if Schwierigkeit == 1:
+		Schuesse = 30
+	if Schwierigkeit == 2:
+		Schuesse = 25
 
 func munition_aufsammeln():
 	Schuesse = Schuesse + randi_range(10,20)
